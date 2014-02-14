0b28abc64769c58cbb5fec040af80a6df3c0cda8
Shmueli Englard <shmueli.yosef@englard.net>
2014-02-13 23:49:33 -0500

    added codePointAt JS file

---
+++ codePointAt.js
@@ -0,0 +1,46 @@
+﻿/*
+    A robust & optimized `String.prototype.codePointAt` polyfill, based on the ECMAScript 6 specification.
+    Based on https://github.com/mathiasbynens/String.prototype.codePointAt
+*/
+window.perfshim("codePointAt", function ()
+{
+    "use strict";
+
+    String.prototype.codePointAt = function (position)
+    {
+        if (this == null)
+        {
+            throw TypeError();
+        }
+        var string = String(this);
+        var size = string.length;
+        // `ToInteger`
+        var index = position ? Number(position) : 0;
+        if (index != index)
+        { // better `isNaN`
+            index = 0;
+        }
+        // Account for out-of-bounds indices:
+        if (index < 0 || index >= size)
+        {
+            return undefined;
+        }
+        // Get the first code unit
+        var first = string.charCodeAt(index);
+        var second;
+        if ( // check if it’s the start of a surrogate pair
+            first >= 0xD800 && first <= 0xDBFF && // high surrogate
+            size > index + 1 // there is a next code unit
+        )
+        {
+            second = string.charCodeAt(index + 1);
+            if (second >= 0xDC00 && second <= 0xDFFF)
+            { // low surrogate
+                // http://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+                return (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000;
+            }
+        }
+        return first;
+    };
+
+});
