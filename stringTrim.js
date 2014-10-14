d40736a11173024ad84540a45e1ce4054376b94e
Shmueli Englard <shmueli.yosef@englard.net>
2014-10-14 13:11:59 -0400

    Added String.trim polyfil

---
+++ stringTrim.js
@@ -0,0 +1,17 @@
+﻿/*
+    stringTrim Shim
+    Based on https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+ */
+window.perfshim("stringTrim", function ()
+{
+    "use strict";
+
+    if (String.prototype.trim === undefined)
+    {
+        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+        String.prototype.trim = function ()
+        {
+            return this.replace(rtrim, "");
+        }
+    }
+});
